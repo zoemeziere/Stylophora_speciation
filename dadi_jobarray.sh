0141c216bdf12e19390a888dadc6f90224445249
Zoe Meziere <71752811+zoemeziere@users.noreply.github.com>
2023-09-26 16:10:39 +1000

    Update dadi_opt_jobarray.sh

--- dadi_jobarray.sh
+++ dadi_jobarray.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-#PBS -A UQ-SCI-BiolSci
-
-#PBS -l select=1:ncpus=2:mem=50GB
-#PBS -l walltime=1:00:00
-#PBS -N nomig_1f
-#PBS -J 1-30:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=40G
+#SBATCH --job-name=nommig_3f
+#SBATCH --time=3:00:00
+#SBATCH --partition=general
+#SBATCH --account=a_riginos
+#SBATCH --array=1-30
+#SBATCH --output=im2m_C1CBHE-C5CBHE_%A_%a.out
 
 #cd ${PBS_O_WORKDIR}
 
-
-params=`cat /scratch/project/phd_coral_genomics/Stylophora/RAD_gbr_genome/dadi/sympatry_allopatry_pairs/C1_allopatry/scripts/nomig_1f_initial_params.txt | tail -n +${PBS_ARRAY_INDEX} | head -1`
+params=`cat /scratch/project/phd_coral_genomics/Stylophora/RAD_gbr_genome/dadi/C1CBHE-C5CBHE/scripts/im2m_params_1f.txt | tail -n +$SLURM_ARRAY_TASK_ID | head -1`
 paramArray=($params) 
 
 # to activate conda
@@ -20,16 +23,15 @@ source /QRISdata/Q5253/miniconda3/etc/profile.d/conda.sh
 conda activate dadi
 
 # to access the directory contains the scripts
-cd /scratch/project/phd_coral_genomics/Stylophora/RAD_gbr_genome/dadi/sympatry_allopatry_pairs/C1_allopatry/scripts/
+cd /scratch/project/phd_coral_genomics/Stylophora/RAD_gbr_genome/dadi/C1CBHE-C5CBHE/scripts/
 
-# run optimisations
-# sanity check of variables
-echo $PBS_ARRAY_INDEX
-echo ${paramArray}
 
 #Parameters
 a=${paramArray[0]}
 b=${paramArray[1]}
 c=${paramArray[2]}
+d=${paramArray[3]}
+e=${paramArray[4]}
+f=${paramArray[5]}
 
-python optimise_manual.py C1CBHE-C1ONLI no_mig yes projection 1 -p $a $b $c
+python optimise_manual.py C1CBHE-C5CBHE IM2M yes projection 3 -p $a $b $c $d $e $f
