d2ba6830c5708d713a8d69f3a5e802d69fc381c7
Zoe Meziere <71752811+zoemeziere@users.noreply.github.com>
2024-01-18 12:52:47 +1000

    Update LRT_sec_contact.py

--- scripts/LRT_sec_contact.py
+++ scripts/LRT_sec_contact.py
@@ -1,10 +1,35 @@
+#!/usr/anaconda3/env/dadi211/bin/python
+# -*- coding: utf-8 -*-
+
+from argparse import Namespace
+from dadi import Numerics, Spectrum, Inference, Godambe
+import demo_models_kp
+import numpy as np
+import argparse
+
+# Likelihood-ratio test comparing nested models, sec_cont_sym_mig and sym_mig
+
+# Define popualtion pair, models and best fit params:
+snps = "C1ONMO-C3ONMO"
+simple_model = demo_models_kp.sym_migration
+complex_model = demo_models_kp.sec_contact_sym_migration
+opt_simple = [5.2312, 5.4377, 0.0535, 5.0676] #simple model
+params_complex = [5.2312, 5.4377, 0.0535, 5.0676, 0] #complex model
+opt_complex = [5.2371, 5.4463, 0.0534, 0.095, 4.9798]
+PTS = [60, 80, 90]
+
+# Define fs:
+fs_path = "../data/fs/{}_projected.fs".format(snps)
+fs = Spectrum.from_file(fs_path)
+ns = fs.sample_sizes
+
 fs.mask[1, 0] = True
 fs.mask[0, 1] = True
 fs.mask[2, 0] = True
 fs.mask[0, 2] = True
 fs.mask[1, 1] = True
 
-# Models:
+# Mmodels:
 func_ex_simple = Numerics.make_extrap_log_func(simple_model)
 sim_simple_model = func_ex_simple(opt_simple, ns, PTS)
 ll_simple_model = Inference.ll_multinom(sim_simple_model, fs)
